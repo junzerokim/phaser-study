e09444f8adf9bcff786f3acb1d49c6823b5040f1
jykim2 <jykim2@xlsoft.co.kr>
2023-08-11 08:52:12 +0900

    Add: Collider

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -1,46 +1,26 @@
 import Phaser from 'phaser';
+import PlayScene from './scenes/PlayScene';
 
-const config = {
-  // WebGL (Web Graphics Library) JS API for rendering 2D and 3D graphics
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    // Arcade physics plugin, manages physics simulation
-    default: 'arcade',
-  },
-  scene: {
-    preload,
-    create,
-    // update,
-  },
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION,
 };
 
-// Loading assets, such as images, music, animations ...
-function preload() {
-  // 'this' context - scene
-  // contains functions and properties we can use
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-}
-
-let bird;
-
-function create() {
-  // x, y, key of the image
-  this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  // middle of the height, 1/10 width
-  bird = this.physics.add
-    .sprite(config.width * 0.1, config.height / 2, 'bird')
-    .setOrigin(0);
-  bird.body.gravity.y = 100;
-}
-
-// 60 fps
-// 60 times per second
-// 60 * 16ms = 1000ms
-function update(time, delta) {
-  console.log(bird.body.velocity.y);
-}
+const config = {
+  type: Phaser.AUTO,
+  ...SHARED_CONFIG,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: [new PlayScene(SHARED_CONFIG)],
+};
 
 new Phaser.Game(config);
