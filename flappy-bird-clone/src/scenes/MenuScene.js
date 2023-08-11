d01f05737134777e49cc7e431a03423a6c0e658a
junzerokim <junzerokim@gmail.com>
2023-08-11 16:48:55 +0900

    Add: Score Menu

--- flappy-bird-clone/src/scenes/MenuScene.js
+++ flappy-bird-clone/src/scenes/MenuScene.js
@@ -7,13 +7,35 @@ class MenuScene extends BaseScene {
     this.menu = [
       { scene: 'PlayScene', text: 'Play' },
       { scene: 'ScoreScene', text: 'Score' },
+      { scsne: null, text: 'Exit' },
     ];
   }
 
   create() {
     super.create();
 
-    this.createMenu(this.menu);
+    this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+  }
+
+  setupMenuEvents(menuItem) {
+    const textGO = menuItem.textGO;
+    textGO.setInteractive();
+    console.log(this);
+    textGO.on('pointerover', () => {
+      textGO.setStyle({ fill: '#ff0' });
+    });
+
+    textGO.on('pointerout', () => {
+      textGO.setStyle({ fill: '#fff' });
+    });
+
+    textGO.on('pointerup', () => {
+      menuItem.scene && this.scene.start(menuItem.scene);
+
+      if (menuItem.text === 'Exit') {
+        this.game.destroy(true);
+      }
+    });
   }
 }
 
