12dff5b7fd20811df9a7dd620335813bd54781d6
jykim2 <jykim2@xlsoft.co.kr>
2023-08-11 11:18:41 +0900

    Add: Pause Button

--- flappy-bird-clone/src/scenes/PlayScene.js
+++ flappy-bird-clone/src/scenes/PlayScene.js
@@ -23,6 +23,7 @@ class PlayScene extends Phaser.Scene {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('bird', 'assets/bird.png');
     this.load.image('pipe', 'assets/pipe.png');
+    this.load.image('pause', 'assets/pause.png');
   }
 
   create() {
@@ -31,6 +32,7 @@ class PlayScene extends Phaser.Scene {
     this.createPipes();
     this.createColliders();
     this.createScore();
+    this.createPause();
     this.handleInputs();
   }
 
@@ -85,6 +87,19 @@ class PlayScene extends Phaser.Scene {
     this.add.text(16, 52, `Best Score: ${bestScore || 0}`, {
       fontSize: '18px',
       fill: '#000',
+    });
+  }
+
+  createPause() {
+    const pauseButton = this.add
+      .image(this.config.width - 10, this.config.height - 10, 'pause')
+      .setInteractive()
+      .setScale(3)
+      .setOrigin(1, 1);
+
+    pauseButton.on('pointerdown', () => {
+      this.physics.pause();
+      this.scene.pause();
     });
   }
 
