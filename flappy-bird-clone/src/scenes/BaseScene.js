d212bc0655639fe95a4ee87438f373c8c8a223ca
junzerokim <junzerokim@gmail.com>
2023-08-16 09:27:17 +0900

    Add: Pause Feature

--- flappy-bird-clone/src/scenes/BaseScene.js
+++ flappy-bird-clone/src/scenes/BaseScene.js
@@ -12,6 +12,26 @@ class BaseScene extends Phaser.Scene {
 
   create() {
     this.add.image(0, 0, 'sky').setOrigin(0, 0);
+
+    if (this.config.canGoBack) {
+      const backButton = this.add
+        .image(this.config.width - 10, this.config.height - 10, 'back')
+        .setOrigin(1)
+        .setScale(2)
+        .setInteractive();
+
+      backButton.on('pointerover', () => {
+        backButton.setScale(2.2);
+      });
+
+      backButton.on('pointerout', () => {
+        backButton.setScale(2);
+      });
+
+      backButton.on('pointerup', () => {
+        this.scene.start('MenuScene');
+      });
+    }
   }
 
   createMenu(menu, setupMenuEvents) {
